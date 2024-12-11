bacd3a19569fbc34208e3263c577f2a86f0444ef
root <tomisdebomnl@gmail.com>
2024-12-11 02:35:09 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	"os"
+)
+
+func AppendCharacterToFile(filename string, char rune) error {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(string(char))
+	if err != nil {
+		return fmt.Errorf("failed to write to file: %w", err)
+	}
+
+	return nil
+}
+func getChar(brightness int) string {
+	brightness = brightness / 10
+	fmt.Println("brightness", brightness)
+	chars := []string{" ", ".", ":", "-", "=", "+", "*", "#", "%", "@"}
+	if brightness >= len(chars) {
+		return "@"
+	}
+
+	switch brightness {
+	case 0:
+		return " "
+	case 1:
+		return "."
+	}
+
+	return chars[brightness]
+}
+
+func main() {
+
+	if len(os.Args) < 1 {
+		fmt.Println("Please provide an image")
+		return
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	bounds := img.Bounds()
+	// var maxBrightness float64 = 42774765
+	var art = make([][]string, bounds.Max.Y)
+	var highest float64 = 0
+	for y := bounds.Min.Y; y < bounds.Max.Y; y += 8 {
+		for x := bounds.Min.X; x < bounds.Max.X; x += 8 {
+			r, g, b, _ := img.At(x, y).RGBA()
+
+			var brightness float64 = (float64(100) / 65535) * ((float64(r) + float64(g) + float64(b)) / 3)
+			if brightness > highest {
+				highest = brightness
+			}
+			fmt.Println(brightness)
+
+			art[y] = append(art[y], getChar(int(brightness)))
+			AppendCharacterToFile("output.txt", rune(getChar(int(brightness))[0]))
+			// fmt.Printf("x=%d, y=%d, r=%d, g=%d, b=%d, a=%d\n", x, y, r, g, b, a)
+
+		}
+		AppendCharacterToFile("output.txt", rune('\n'))
+	}
+	fmt.Println(highest)
+	fmt.Println(art)
+}
